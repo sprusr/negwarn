a7d1541f5388452f4513dc42739bad4e2bdb3b9c
sprusr <sprusr@sprusr.tk>
2016-01-31 12:36:21 +0000

    Fixed a number of things
    Please don't think this is what my commit messages are usually lik

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var config = require('./config.js'); //must export an object with botkey and hodkey
 
+var http = require('http');
+
 var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(config.botkey, {polling: true});
 
@@ -9,41 +11,97 @@ var hod = new havenondemand.HODClient(config.hodkey, 'v1');
 var scores = {};
 
 var negResponse = [
-    'STOP BEING NEGATIVE! 😡',
-    'If you keep up this negativity 🔫, you\'ll probably die young 💣',
-    'Baaaaaaaaaad negativity 😇',
+    'STOP BEING NEGATIVE! 🙉',
+    'Keep up this negativity, and you\'ll probably injure yourself',
+    'Negativity is bad for you 😇',
     'Be more positive! 😬',
     'Maybe that could upset somebody who is more sensitive than you 😭😁'
 ];
 
 var posResponse = [
-    'Calm down, yeah? Bit too positive 😠',
+    'Calm down, yeah? Bit too positive 🐍',
     'Realism is important. Stop being so positive! 😑',
-    'Really glad you\'re trying to be positive! Maybe it\'s possible to be too happy?',
-    'It\'s a shame that they\'re not as positive as you are'
+    'Really glad you\'re trying to be positive! Maybe it\'s possible to be too happy? 🐙',
+    'It\'s a shame that they\'re not as positive as you are '
 ];
+
+var offensive = false;
+
+process.argv.forEach(function (val, index, array) {
+  if(val == '--offensive') {
+    offensive = true;
+  }
+});
 
 bot.onText(/^\/start$/, function (msg, match) {
   bot.sendMessage(msg.chat.id, 'Negativity kills!! But don\'t worry, because I\'m here to stop you from being too negative 🙃');
 });
 
 bot.on('text', function (msg) {
-  console.log(msg);
   hod.call('analyzesentiment', { text: msg.text }, function(err, resp, body) {
     if(!scores[msg.chat.id]) {
-      scores[msg.chat.id] = 0;
+      scores[msg.chat.id] = {
+        score: 0,
+        negThreshold: -2.5,
+        posThreshold: 2.5,
+        resetPercent: 0,
+        thresholdResetPercent: 120,
+        offensiveMode: offensive
+      };
     }
-    scores[msg.chat.id] += body.aggregate.score;
-    console.log('Text: ' + msg.text);
+    scores[msg.chat.id].score += body.aggregate.score;
+    console.log('From: ' + msg.from.first_name + ' Text: ' + msg.text);
     console.log(msg.chat.id + ' last score: ' + body.aggregate.score);
     console.log(msg.chat.id + ' total score: ' + scores[msg.chat.id]);
-    if(scores[msg.chat.id] < -2) {
-      bot.sendMessage(msg.chat.id, negResponse[Math.floor(Math.random()*negResponse.length)]);
-      scores[msg.chat.id] = 0;
+    if(scores[msg.chat.id].score < scores[msg.chat.id].negThreshold) {
+      getNeg(scores[msg.chat.id].offensiveMode, function(neg) {
+        bot.sendMessage(msg.chat.id, neg);
+        scores[msg.chat.id].score = (scores[msg.chat.id].score/100)*scores[msg.chat.id].resetPercent;
+        scores[msg.chat.id].negThreshold = (scores[msg.chat.id].negThreshold/100)*scores[msg.chat.id].thresholdResetPercent;
+      });
     }
     if(scores[msg.chat.id] > 2) {
-      bot.sendMessage(msg.chat.id, posResponse[Math.floor(Math.random()*posResponse.length)]);
-      scores[msg.chat.id] = 0;
+      getPos(function(pos) {
+        bot.sendMessage(msg.chat.id, pos);
+        scores[msg.chat.id].score = (scores[msg.chat.id].score/100)*scores[msg.chat.id].resetPercent;
+        scores[msg.chat.id].posThreshold = (scores[msg.chat.id].posThreshold/100)*scores[msg.chat.id].thresholdResetPercent;
+      });
     }
   });
 });
+
+bot.onText(/^\/toggleoffensive$/, function (msg, match) {
+  var res;
+  scores[msg.chat.id].offensiveMode = !scores[msg.chat.id].offensiveMode;
+  if(scores[msg.chat.id].offensiveMode) {
+    res = 'Offensive mode ON!'
+  } else {
+    res = 'Offensive mode OFF!'
+  }
+  bot.sendMessage(msg.chat.id, res);
+});
+
+var getPos = function(cb) {
+  cb(posResponse[Math.floor(Math.random()*posResponse.length)]);
+};
+
+var getNeg = function(offensive, cb) {
+  if(offensive) {
+    http.request({
+      host: 'offensive.site',
+      path: '/insult'
+    }, function(res) {
+      var str = '';
+
+      res.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      res.on('end', function () {
+        cb(str);
+      });
+    }).end();
+  } else {
+    cb(negResponse[Math.floor(Math.random()*negResponse.length)]);
+  }
+};
